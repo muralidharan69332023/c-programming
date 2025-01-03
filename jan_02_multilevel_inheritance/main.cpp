0b6f075c7274871f56423e1976b3ee8f235bdbef
muralidharan69332023 <muralidharan@merituseducation.com>
2025-01-03 15:45:46 +0530

    multilevel_inheritance

---
+++ jan_02_multilevel_inheritance/main.cpp
@@ -0,0 +1,64 @@
+//#include <iostream>
+//using namespace std;
+//
+//// Parent class
+//class MyClass {
+//  public:
+//    void myFunction() {
+//      cout << "Some content in parent class. \n" ;
+//    }
+//    void agla() {
+//      cout << "next\n" ;
+//    }
+//};
+//
+//// Child class
+//class MyChild: public MyClass {
+//    public:
+//    void fir() {
+//      cout << "hindi\n" ;
+//    }
+//};
+//
+//// Grandchild class
+//class MyGrandChild: public MyChild {
+//    public:
+//    void haftha() {
+//      cout << "week\n" ;
+//    }
+//};
+//
+//int main() {
+//
+//  MyGrandChild myObj;
+//  myObj.agla();
+//  myObj.myFunction();
+//  myObj.fir();
+//   myObj.haftha();
+//
+//  return 0;
+//}
+#include <iostream>
+using namespace std;
+
+// Parent class
+class MyClass {
+  public:
+    void myFunction() {
+      cout << "Some content in parent class." ;
+    }
+};
+
+// Child class
+class MyChild: public MyClass {
+};
+
+// Grandchild class
+class MyGrandChild: public MyClass{
+};
+
+int main() {
+  MyGrandChild myObj;
+  myObj.myFunction();
+  return 0;
+}
